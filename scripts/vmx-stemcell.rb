39a070d41446986e31fba54d2335bef37faa8fe2
David Jahn <djahn@pivotal.io>
2017-01-26 17:23:51 -0500

    wip: working VMX builder
    Uses vmware fusion on Laurel, starts with a fresh Windows install
    with vmware tools installed and winrm enabled.
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

---
+++ scripts/vmx-stemcell.rb
@@ -0,0 +1,115 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'zlib'
+require 'tmpdir'
+require 'open3'
+require 'securerandom'
+require 'pathname'
+require 'mkmf'
+require_relative '../erb_templates/templates.rb'
+
+# concourse inputs
+VERSION = File.read("version/number").chomp
+
+AGENT_PATH = "compiled-agent/agent.zip"
+AGENT_DEPS_PATH = "compiled-agent/agent-dependencies.zip"
+AGENT_COMMIT = File.read("compiled-agent/sha").chomp
+
+OUTPUT_DIR = ENV.fetch("OUTPUT_DIR")
+MEMSIZE = ENV.fetch('MEMSIZE', '4096')
+NUMVCPUS = ENV.fetch('NUMVCPUS', '4')
+ADMINISTRATOR_PASSWORD = ENV.fetch('ADMINISTRATOR_PASSWORD')
+
+def gzip_file(name, output)
+  Zlib::GzipWriter.open(output) do |gz|
+   File.open(name) do |fp|
+     while chunk = fp.read(32 * 1024) do
+       gz.write chunk
+     end
+   end
+   gz.close
+  end
+end
+
+def packer_command(command, config_path)
+  Dir.chdir(File.dirname(config_path)) do
+
+    args = %{
+      packer #{command} \
+      -var "administrator_password=#{ADMINISTRATOR_PASSWORD}" \
+      #{config_path}
+    }
+    Open3.popen2e(args) do |stdin, stdout_stderr, wait_thr|
+      stdout_stderr.each_line do |line|
+        puts line
+      end
+      exit_status = wait_thr.value
+      if exit_status != 0
+        puts "packer failed #{exit_status}"
+        exit(1)
+      end
+    end
+  end
+end
+
+def exec_command(cmd)
+  `#{cmd}`
+  exit 1 unless $?.success?
+end
+
+if find_executable('packer') == nil
+  abort("ERROR: cannot find 'packer' on the path")
+end
+
+# find sha1sum executable name
+# SHA1SUM='sha1sum'
+# if find_executable(SHA1SUM) == nil
+#   SHA1SUM='shasum' # OS X
+#   if find_executable(SHA1SUM) == nil
+#     abort("ERROR: cannot find 'sha1sum' or 'sha1sum' on the path")
+#   end
+# end
+
+# FileUtils.mkdir_p(OUTPUT_DIR)
+# output_dir = File.absolute_path(OUTPUT_DIR)
+
+# IMAGE_PATH = "#{output_dir}/image"
+
+BUILDER_PATH=File.expand_path("../..", __FILE__)
+
+packer_config = File.join(BUILDER_PATH, "vsphere", "packer-from-vmx.json")
+
+packer_command('validate', packer_config)
+packer_command('build', packer_config)
+
+# ova_file = Dir.glob('**/packer-vmware-iso.ova' ).select { |fn| File.file?(fn) }
+# if ova_file.length == 0
+#   abort("ERROR: unable to find packer-vmware-iso.ova")
+# end
+#
+# # remove network interface from VM image
+# Dir.mktmpdir do |dir|
+#   exec_command("tar xf #{ova_file[0]} -C #{dir}")
+#   f = Nokogiri::XML(File.open("#{dir}/packer-vmware-iso.ovf"))
+#   f.css("VirtualHardwareSection Item").select {|x| x.to_s =~ /Ethernet 1/}.first.remove
+#   File.write("#{dir}/packer-vmware-iso.ovf", f.to_s)
+#   ova_file_path = File.absolute_path(ova_file[0])
+#   Dir.chdir(dir) do
+#     exec_command("tar cf #{ova_file_path} packer-vmware-iso.ovf packer-vmware-iso.mf packer-vmware-iso-disk1.vmdk")
+#   end
+# end
+#
+# gzip_file(ova_file[0], "#{IMAGE_PATH}")
+#
+# IMAGE_SHA1=`#{SHA1SUM} #{IMAGE_PATH} | cut -d ' ' -f 1 | xargs echo -n`
+#
+# Dir.mktmpdir do |dir|
+#   MFTemplate.new("#{BUILDER_PATH}/erb_templates/vsphere/stemcell.MF.erb", VERSION, sha1: IMAGE_SHA1).save(dir)
+#   ApplySpecTemplate.new("#{BUILDER_PATH}/erb_templates/apply_spec.yml.erb", AGENT_COMMIT).save(dir)
+#   FileUtils.cp("#{IMAGE_PATH}", dir)
+#
+#   stemcell_filename = "bosh-stemcell-#{VERSION}-vsphere-esxi-windows2012R2-go_agent.tgz"
+#
+#   exec_command("tar czvf #{File.join(output_dir, stemcell_filename)} -C #{dir} stemcell.MF apply_spec.yml image")
+# end
