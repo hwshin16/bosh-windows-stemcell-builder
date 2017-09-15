75cbe4216e48ce0eee79871dfb5f36a364760326
David Jahn <djahn@pivotal.io>
2017-09-15 12:09:28 -0400

    Fix lgpo invocation
    Start lgpo in a process and wait for its completion
    
    [#150861392](https://www.pivotaltracker.com/story/show/150861392)
    
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.psm1
+++ bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.psm1
@@ -1,4 +1,19 @@
-﻿<#
+﻿function Run-Lgpo {
+    Param (
+        [string]$LgpoExe ="C:\windows\lgpo.exe",
+        [string]$LogDir,
+        [string]$ArgumentList
+    )
+    New-Item -Path $LogDir -ItemType Directory -Force
+    Start-Process -Filepath $LgpoExe -ArgumentList $ArgumentList -Wait `
+        -RedirectStandardOutput $LogDir\stdout.txt `
+        -RedirectStandardError $LogDir\stderr.txt
+    if ($? -ne $True) {
+        Throw "lgpo.exe completed with errors"
+    }
+}
+
+<#
 .Synopsis
     Sysprep Utilities
 .Description
@@ -13,35 +28,10 @@ function Enable-LocalSecurityPolicy {
 
     Write-Log "Starting LocalSecurityPolicy"
 
-    # Get the current default values
-
-    ## Get LGPO backup
-    $LGPOBackup = "C:\bosh\lgpo-backup"
-    New-Item -Path $LGPOBackup -ItemType Directory -Force
-    Invoke-Expression "$LgpoExe /b $LGPOBackup"
-    $LGPOBackupInfContent = Get-Content "$LGPOBackup\*\DomainSysvol\GPO\Machine\microsoft\windows nt\SecEdit\GptTmpl.inf"
-
-    $MinimumPasswordAge = $LGPOBackupInfContent | Select-String "MinimumPasswordAge"
-    $EnableAdminAccount = $LGPOBackupInfContent | Select-String "EnableAdminAccount"
-
-    ## Get current default values from registry
-
-    $EnableICMPRedirect = (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters").EnableICMPRedirect
-    # $AdmPwdEnabled = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft Services\AdmPwd").AdmPwdEnabled
-    # $PasswordAgeDays = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft Services\AdmPwd").PasswordAgeDays
-    # $PasswordComplexity = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft Services\AdmPwd").PasswordComplexity
-    # $PasswordLength = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft Services\AdmPwd").PasswordLength
-    # $PwdExpirationProtectionEnabled = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft Services\AdmPwd").PwdExpirationProtectionEnabled
-    # $PolicyVersion = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft\WindowsFirewall").PolicyVersion
-    # $fMinimizeConnections = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WcmSvc\GroupPolicy").fMinimizeConnections
-    # $AUOptions = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU").AUOptions
-    # $NoAutoUpdate = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU").NoAutoUpdate
-    # $ScheduledInstallDay = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU").ScheduledInstallDay
-    # $ScheduledInstallTime = (Get-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU").ScheduledInstallTime
-
     # Apply MSL1
+
     $policyZipFile = Join-Path $PSScriptRoot "cis-policy-baseline.zip"
-    New-Item -Path "C:\bosh\lgpo" -ItemType Directory -Force
+    New-Item -Path $PolicyDestination -ItemType Directory -Force
     Open-Zip -ZipFile $policyZipFile -OutPath $PolicyDestination
 
     if($EnableRDP) {
@@ -49,29 +39,31 @@ function Enable-LocalSecurityPolicy {
         ModifyInfFile -InfFilePath $InfFilePath -KeyName "SeDenyNetworkLogonRight" -KeyValue "*S-1-5-32-546"
     }
 
-    Invoke-Expression "$LgpoExe /g $PolicyDestination /v 2>&1 > $PolicyDestination\LGPO.log"
-    if ($LASTEXITCODE -ne 0) {
-        Throw "lgpo.exe exited with $LASTEXITCODE"
-    }
+    Run-Lgpo -ArgumentList "/g $PolicyDestination /v" -LogDir $PolicyDestination\CIS-MSL1-Logs
 
     # Reapply the current default values
 
     $LGPODefaultsDirectory = "C:\bosh\lgpo-defaults"
     New-Item -Path $LGPODefaultsDirectory -ItemType Directory -Force
     $InfFileContents=@"
+[Unicode]
+Unicode=yes
+[Version]
+signature=`$CHICAGO`$
+Revision=1
+[Registry Values]
 [System Access]
-$MinimumPasswordAge
-$EnableAdminAccount
+MinimumPasswordAge=0
+EnableAdminAccount=1
+[Privilege Rights]
 "@
+    $InfFileContents | Out-File -FilePath "$LGPODefaultsDirectory\defaults.inf" -Encoding unicode -Force
 
-    Out-File -FilePath "$LGPODefaultsDirectory\defaults.inf" -Encoding unicode -InputObject $InfFileContents -Force
-    Invoke-Expression "$LgpoExe /s $LGPODefaultsDirectory\defaults.inf"
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "LGPO.exe exited with non-zero code: ${LASTEXITCODE}"
-        Exit $LASTEXITCODE
-    }
+    Run-Lgpo -ArgumentList "/s $LGPODefaultsDirectory\defaults.inf" -LogDir $PolicyDestination\Undo-MSL1-Logs
 
-    Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" "EnableICMPRedirect" $EnableICMPRedirect
+    Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" "EnableICMPRedirect" 1
+
+    # Removing L1 registry keys that may break the BOSH stemcell (currently unknown if they do)
     Remove-ItemProperty "HKLM:\Software\Policies\Microsoft Services\AdmPwd" "AdmPwdEnabled"
     Remove-ItemProperty "HKLM:\Software\Policies\Microsoft Services\AdmPwd" "PasswordAgeDays"
     Remove-ItemProperty "HKLM:\Software\Policies\Microsoft Services\AdmPwd" "PasswordComplexity"
